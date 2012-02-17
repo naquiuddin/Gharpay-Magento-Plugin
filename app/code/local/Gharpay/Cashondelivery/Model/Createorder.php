bf42fdfa7817e3edfe5bdbe78a61255a382cb515
khaja Naquiuddin <khaja@gharpay.in>
2012-02-17 08:14:42 +0530

    Database and Admin side Configs added

--- app/code/local/Gharpay/Cashondelivery/Model/Createorder.php
+++ app/code/local/Gharpay/Cashondelivery/Model/Createorder.php
@@ -2,37 +2,19 @@
 include_once 'app/Mage.php';
 require_once(Mage::getBaseDir('lib').'\Gharpay\Array2Xml.php');
 require_once(Mage::getBaseDir('lib').'\Gharpay\Xml2Array.php');
-//class ZendHttp extends Zend_Http_Response
-//{
-//
-//    public static function decodeChunkedBody($body)
-//    {
-//    $decBody = '';
-//    if (preg_match("/^([\da-fA-F]+)[^\r\n]*\r\n/sm", trim($body))) {
-//        while (preg_match("/^([\da-fA-F]+)[^\r\n]*\r\n/sm", trim($body), $m)) {
-//            $length = hexdec(trim($m[1]));
-//            $cut = strlen($m[0]);
-//
-//            $decBody .= substr($body, $cut, $length);
-//            $body = substr($body, $cut + $length + 2);
-//        }
-//    } else {
-//        return $body;
-//    }
-//
-//    return $decBody;
-//    }   
-//}
+require_once(Mage::getModuleDir('Model', 'Gharpay_Dbconns').'\Model\Gharpayorders.php');
+
 class Gharpay_Cashondelivery_Model_Createorder extends Mage_Payment_Model_Method_Abstract
-{	
+{
     protected $_code = 'cashondelivery';
     protected $_canCapture = true;
     protected $_canUseCheckout = true;
     protected $_canFetchTransactionInfo     = true;
     protected $_isGateway                   = true;
+    
     public function validate()
     {
-         $paymentInfo = $this->getInfoInstance();
+       $paymentInfo = $this->getInfoInstance();
          if ($paymentInfo instanceof Mage_Sales_Model_Order_Payment) {
              $postCode = $paymentInfo->getOrder()->getBillingAddress()->getPostcode();
              Mage::log('Validate Pincode :'.$postCode.'  In GetOrder');
@@ -50,12 +32,15 @@ class Gharpay_Cashondelivery_Model_Createorder extends Mage_Payment_Model_Method
     
     public function canUseForPostCode($postCode)
     {
-        
+        $uri = Mage::getStoreConfig('payment/cashondelivery/gharpay_uri',Mage::app()->getStore());
+        $username = Mage::getStoreConfig('payment/cashondelivery/username',Mage::app()->getStore());
+        $password = Mage::getStoreConfig('payment/cashondelivery/password',Mage::app()->getStore());
+        Mage::Log($uri);Mage::Log($username);Mage::Log($password); 
         Mage::app(); //for autoloading:)
-        $client = new Varien_Http_Client('http://services.gharpay.in/rest/GharpayService/isPincodePresent?pincode='.$postCode);
+        $client = new Varien_Http_Client('http://'.$uri.'/rest/GharpayService/isPincodePresent?pincode='.$postCode);
         $client->setMethod(Varien_Http_Client::GET);
-        $client->setHeaders('username','redbus');
-        $client->setHeaders('password','redbus');
+        $client->setHeaders('username',$username);
+        $client->setHeaders('password',$password);
         $client->setEncType('application/xml');
         /** @var $response Zend_Http_Response */
         $response = $client->request();
@@ -70,12 +55,16 @@ class Gharpay_Cashondelivery_Model_Createorder extends Mage_Payment_Model_Method
     }
     public function capture(Varien_Object $payment, $amount)
     {
+        $uri = Mage::getStoreConfig('payment/cashondelivery/gharpay_uri',Mage::app()->getStore());
+        $username = Mage::getStoreConfig('payment/cashondelivery/username',Mage::app()->getStore());
+        $password = Mage::getStoreConfig('payment/cashondelivery/password',Mage::app()->getStore());
+        $date=new DateTime();
+        Mage::Log($uri);Mage::Log($username);Mage::Log($password);
             $paymentInfo = $this->getInfoInstance();
             Mage::Log('Got Order Info Instance');
             Mage::Log($amount);
             $order = $paymentInfo->getOrder();
             $info=$order->getBillingAddress();
-            Mage::Log($this->getCofigData('payment_action')); 
            $customerDetails = array(
                 "address"=>$info->getStreetFull().','.$info->getRegion().','.$info->getCity().','.$info->getCountry(),
                 "contactNo"=>$info->getTelephone(),
@@ -98,7 +87,7 @@ class Gharpay_Cashondelivery_Model_Createorder extends Mage_Payment_Model_Method
             $orderDetails = array(
             "pincode"=>$order->getBillingAddress()->getPostcode(),
             "clientOrderID"=>$order->getIncrementId(),
-            "deliveryDate"=>date('d-m-Y'),
+            "deliveryDate"=>$date->format('d-m-Y'),
             "orderAmount"=>$order->getBaseGrandTotal(),
             "productDetails"=>$productDetails
             );
@@ -111,15 +100,45 @@ class Gharpay_Cashondelivery_Model_Createorder extends Mage_Payment_Model_Method
                  $xml=$xml->saveXML();
                  Mage::Log('Log XML request :'.$xml);                        
                  Mage::app(); //for autoloading:)
-                 $client = new Varien_Http_Client('http://services.gharpay.in/rest/GharpayService/createOrder');
-                 $client->setHeaders('username','redbus');
-                 $client->setHeaders('password','redbus');
+                 $client = new Varien_Http_Client('http://'.$uri.'/rest/GharpayService/createOrder');
+                 $client->setHeaders('username',$username);
+                 $client->setHeaders('password',$password);
                  $client->setMethod(Varien_Http_Client::POST);
                  $client->setRawData($xml, 'application/xml');
                  /** @var $response Zend_Http_Response */
                  $response = $client->request();
-                 Mage::Log($response);
-        return $this;
+                 Mage::Log('Log Response :'.$response);
+                 $resXML=$response->getRawBody();
+                 Mage::Log('Log Response Body XML :'.$resXML);
+                 $resArr=XML2Array::createArray($resXML);
+                 if(($response->getStatus()==200)&&!isset($resArr['createOrderResponse']['errorCode']))
+                 {
+                     Mage::Log($response->getStatus());
+                     Mage::Log($response->getStatus());
+                     Mage::Log($response->isError());
+                     Mage::Log($response->isSuccessful());
+                    $clientId=$resArr['createOrderResponse']['clientOrderID'];
+                    if($clientId==$order->getIncrementId())
+                    {
+                        Mage::Log('Log response body Client ID: '.$clientId);
+                        $gharpayId=$resArr['createOrderResponse']['orderID'];
+                        Mage::Log('Log Gharpay ID: '.$gharpayId);
+                        $gharpayorders= new Gharpay_Dbconns_Model_Gharpayorders();
+                        $gharpayorders->setGharpayOrderId($gharpayId);
+                        $gharpayorders->setClientOrderId($clientId);
+                        
+                        $gharpayorders->setCreatedAt($date->format('Y-m-d H:i:s'));
+                        $gharpayorders->setUpdatedAt($date->format('Y-m-d H:i:s'));
+                        $gharpayorders->save();
+                        Mage::Log('saved successfully');
+                    }
+                 }
+                 else
+                 {
+                     Mage::Log($response->getStatus());
+                     Mage::throwException($this->_getHelper()->__('Oops! Some error occured in the Payment Gateway, Please try after some time or Contact Us'));                     
+                 }
+            return $this;
     }     
 }
 ?>
